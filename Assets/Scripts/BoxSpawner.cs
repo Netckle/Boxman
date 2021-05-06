6b3190f9262eb01d907f49ed2a847e8be734d7db
netckle <33153152+netckle@users.noreply.github.com>
2021-05-06 19:29:01 +0900

    21.05.06

---
+++ Assets/Scripts/BoxSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxSpawner : MonoBehaviour {
+
+    public GameObject box;
+    public bool isSpawned = true;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (isSpawned)
+        {
+            isSpawned = false;
+            Instantiate(box, transform);
+        }
+    }
+
+}
